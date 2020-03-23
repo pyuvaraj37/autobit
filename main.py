30aadc19df43e7e50784226c5af9090927ef758f
pyuvaraj37 <ryuvaraj37@gmail.com>
2020-03-22 22:35:17 -0700

    Init

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+import robin_stocks as r 
+
+
+#main
+login = r.login('yrenter@gmail.com','1y6a8s1w3a1n')
+usr_stocks = r.build_holdings()
+btc_history = r.crypto.get_crypto_history('BTC','day', 'year', '24_7')
+
+for key, value in btc_history.items():
+    if key != 'data_points':
+        print(key, value)
+    else: 
+        print(len(value))
+
+
+
