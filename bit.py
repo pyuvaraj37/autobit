9aae0ccd7d75f020f51c12582510461b7aed7709
pyuvaraj37 <ryuvaraj37@gmail.com>
2020-03-26 12:39:47 -0700

    Adding class structure

---
+++ bit.py
@@ -0,0 +1,40 @@
+import robin_stocks as r
+
+class Bit(object):
+    username = ''
+    password = ''
+    interval = 0
+    data = []
+
+    def __init__(self,username, password):
+        self.username = username
+        self.password = password
+    
+    def set_historical_window(self, i):
+        self.interval = i
+
+    def get_moving_average(self):
+        data = self.get_formated_historical()
+        sum = 0
+        for data_point in data:
+            sum += float(data_point['close_price'])
+        sum /= len(data)
+        return sum
+
+    #interval range 5 - 200 minute
+    def get_formated_historical(self):
+
+        interval = int(self.interval / 5)
+        btc_history = self.get_historical()
+        data = btc_history['data_points']
+        if interval is 1:
+            return data 
+        interval_data = data[::interval]
+        return interval_data
+
+    def get_historical(self):
+        login = r.login(self.username, self.password)
+        btc_history = r.crypto.get_crypto_historicals('BTC', '5minute', 'day', '24_7')
+        login = r.logout(); 
+        return btc_history
+
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import robin_stocks as r 
-
+import tkinter as t
+from bit import Bit
 
 #main
-login = r.login('yrenter@gmail.com','1y6a8s1w3a1n')
+# login = r.login('yrenter@gmail.com','1y6a8s1w3a1n')
 
-btc_history = r.crypto.get_crypto_history('BTC','5minute', 'day', '24_7')
-interval = btc_history['data_points']
-last50min = interval[276:287]
-data_point_close_price = []
-for data_point in last50min:
-    data_point_close_price.append(float(data_point['close_price']))
+b = Bit('yrenter@gmail.com','1y6a8s1w3a1n')
+b.set_historical_window(5)
+btc_ma = b.get_moving_average()
+print(btc_ma)
 
-Data = {
-    'Times' : [-50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0],
-    'Value' : data_point_close_price
-}
+# btc_history = r.crypto.get_crypto_historicals('BTC','5minute', 'day', '24_7')
+# interval = btc_history['data_points']
+# last50min = interval[276:287]
+# data_point_close_price = []
+# for data_point in last50min:
+#     print(data_point)
 
-df = DataFrame(Data,columns=['Times','Value'])
-df.plot(x ='Times', y='Value', kind = 'line')
-plt.show()
+# Data = {
+#     'Times' : [-50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0],
+#     'Value' : data_point_close_price
+# }
+
+# df = DataFrame(Data,columns=['Times','Value'])
+# df.plot(x ='Times', y='Value', kind = 'line')
+# plt.show()
+
+# root = t.Tk() 
+# menu = t.Menu(root) 
+# root.config(menu=menu) 
+# filemenu = t.Menu(menu) 
+# menu.add_cascade(label='Intervals', menu=filemenu) 
+# filemenu.add_command(label='1 minute', command=) 
+# filemenu.add_command(label='5 minute') 
+# filemenu.add_separator() 
+# filemenu.add_command(label='Exit', command=root.quit) 
+# helpmenu = t.Menu(menu) 
+# menu.add_cascade(label='Help', menu=helpmenu) 
+# helpmenu.add_command(label='About') 
+# t.mainloop() 
